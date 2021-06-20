bbd4700fe152718419948163c5c90c29a559610a
alex <alexramsdell@gmail.com>
2021-06-20 18:36:35 -0400

    wordstats + mongo queries.

---
+++ week5/WordStats.py
@@ -0,0 +1,25 @@
+'''
+
+    A class implimentation of week 4's word stat script.
+
+'''
+
+class WordStat:
+
+    def __init__(self, filenames):
+        self.filenames = filenames
+
+    def read_files(self):
+        pass
+
+    def get_most_frequent(self):
+        pass
+
+    def get_least_frequent(self):
+        pass
+    
+    def run(self):
+        pass
+
+    def format_stats(self, stat_name, data):
+        pass
